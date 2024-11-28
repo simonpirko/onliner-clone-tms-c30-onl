d006b2f5beea83c4270d740e5b48523fef0c55cd
nick_tsygankov <kolanberat@gmail.com>
2024-11-28 21:13:18 +0300

    Внесли правки

--- src/main/java/by/tms/onlinerclonec30onl/Constants.java
+++ src/main/java/by/tms/onlinerclonec30onl/Constants.java
@@ -3,7 +3,7 @@ package by.tms.onlinerclonec30onl;
 public interface Constants {
     String PROJECT_ROOT_PATH = "by.tms.onlinerclonec30onl";
     String APPLICATION_PROPERTIES_PATH = "classpath:application.properties";
-    String PRODUCT_TYPES_CONTEXT = "/productstypes";
+    String PRODUCT_TYPES_CONTEXT = "/catalog";
     String UPDATE_CONTEXT = "/admin/update";
     String CREATE_CONTEXT = "/admin/create";
     String DELETE_CONTEXT = "/admin/delete";
